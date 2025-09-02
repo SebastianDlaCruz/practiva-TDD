90f4a7b7683145fa6634d4aad51203ce01351962
SebastianDlaCruz <sebastiandelacruz335@linktic.com>
2025-09-02 17:57:32 -0300

    validacion de negativos y decimales

--- src/fizzbuzz/fizzbuzz.test.ts
+++ src/fizzbuzz/fizzbuzz.test.ts
@@ -14,11 +14,17 @@ describe("fizzbuzz", () => {
     expect(fizzBuzz(15)).toBe("fizzbuzz");
   })
 
-  test("que no sean string", () => {
+  test("que sea divisible por 3 y 5", () => {
     expect(() => fizzBuzz(2)).toThrowError("El numero debe ser divisible por 3 y por 5")
   })
 
-  test("si los números son negativos", () => { })
+  test("si los números son negativos", () => {
+    expect(fizzBuzz(-3)).toBe("fizz");
+    expect(fizzBuzz(-5)).toBe("buzz");
+    expect(fizzBuzz(15)).toBe("fizzbuzz");
+  })
 
-  test("si los números son decimales", () => { })
+  test("si los números son decimales", () => {
+    expect(fizzBuzz(3.0)).toBe("fizz");
+  })
 });
--- src/fizzbuzz/fizzbuzz.ts
+++ src/fizzbuzz/fizzbuzz.ts
@@ -2,11 +2,13 @@
 
 export const fizzBuzz = (number: number): string => {
 
+
   if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz';
 
   if (number % 3 === 0) return "fizz";
 
   if (number % 5 === 0) return 'buzz';
+
 
   throw new Error("El numero debe ser divisible por 3 y por 5");
 }
